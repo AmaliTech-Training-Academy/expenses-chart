11fe2b94d745028063c3a8fa05726f9113b8d8fa
NeonHayford <hayford.lumorvi@gmail.com>
2023-01-17 09:41:33 +0000

    commit

--- scripts.js
+++ scripts.js
@@ -5,7 +5,9 @@ function chartItem(item) {
     return `
         <div class="chart-card">
           <button class="chart-btn">
-              <div class="chart-item" style="height: ${item.amount *3}px;"></div>
+              <div class="chart-item" style="height: ${item.amount *3.75}px;">
+                <p class="chart-item-tooltip">$${item.amount}</p>
+              </div>
               <p class="chart-item-day">${item.day}</p>
           </button>
           <p class="chart-item-tooltip">$${item.amount}</p>
@@ -13,24 +15,20 @@ function chartItem(item) {
     `;
 }
 
-
-// chartdiv.innerHTML = `
-//   <button class="chart-btn">
-//       <div class="chart-item"></div>
-//       <p class="chart-item-day">${querydata.day}</p>
-//   </button>
-//   <p class="chart-item-tooltip">${querydata.amount}</p>
-// `
-
 async function chart(){
     const chartdata= await fetch('./data.json');
     const data= await chartdata.json();
     console.log(data);
+    // let amountArr = data.map(item => item.amount);
+    // const index = amountArr.indexOf(Math.max(amountArr));
+    // console.log(data[index]);
     // list_ = JSON.parse(data)
      chartEl.innerHTML = data
-    data
+    // data
     // console.log(data[1].amount)
     chartEl.innerHTML = data.map(i => chartItem(i));
 }
 chart()
 
+// document.querySelectorAll('.chart-card')
+// Number([...document.querySelectorAll('.chart-item')][0].style.height.split('.')[0])
